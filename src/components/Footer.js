0ec076fd4fffc6a212740fed43284bf6fbdd6382
Ramiro Blas <>
2023-12-17 04:58:15 -0500

    feat: se agrego  nuevo titulo, icono y enlaces a redes sociales

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,19 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="social-icons">
-        <img src="/images/facebook.png" alt="Facebook" />
-        <img src="/images/instagram.png" alt="Instagram" />
-        <img src="/images/twitter.png" alt="Twitter" />
-      </div>
-    </footer>
+      <footer>
+          <div className="social-icons">
+              <a href="https://www.facebook.com/blacknutgames" target="_blank" rel="noopener noreferrer">
+                  <img src="/images/facebook.png" alt="Facebook" />
+              </a>
+              <a href="https://www.instagram.com/gamecenterlatam/" target="_blank" rel="noopener noreferrer">
+                  <img src="/images/instagram.png" alt="Instagram" />
+              </a>
+              <a href="https://twitter.com/Blacknut_Games" target="_blank" rel="noopener noreferrer">
+                  <img src="/images/twitter.png" alt="Twitter" />
+              </a>
+          </div>
+      </footer>
   );
 };
 
